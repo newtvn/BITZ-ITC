a125505621977f8d9abfbee3405c1d3e094b08f7
Newtvn <newtonbryan12428@example.com>
2025-06-23 13:50:27 +0300

    trial

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -3,7 +3,6 @@
 import * as React from "react"
 import { ThemeProvider as NextThemesProvider } from "next-themes"
 
-// Let TypeScript infer the prop types from the real component
 export function ThemeProvider(props: React.ComponentProps<typeof NextThemesProvider>) {
   return <NextThemesProvider {...props} />
 }
